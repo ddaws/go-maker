a1949d0e237b1cfb7cb465fad1af262b34003085
Dawson <ddaws@users.noreply.github.com>
2020-03-04 11:15:58 +0400

    Added bindings to the Jug

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# TODO: Update this script to be position independant
+
 # Generate Vat contact bindings
 abigen --sol=./dss/src/vat.sol --pkg=maker --type=Vat --out=maker/vat.go
 
 # Generate Pot contract bindings
-abigen --sol=./dss/src/pot.sol --pkg=maker --type=Pot  --alias=Pie=TotalPie --out=maker/pot.go
+abigen --sol=./dss/src/pot.sol --pkg=maker --type=Pot --alias=Pie=TotalPie --out=maker/pot.go
+
+# Generate Jug contract bindings
+abigen --sol=./dss/src/jug.sol --pkg=maker --type=Jug --alias=vat=VatAddress,vow=VowAddress --out=./maker/jug.go
