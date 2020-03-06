b5a779233d4844af3221a752cc8bdddf4f044446
Dawson <ddaws@users.noreply.github.com>
2020-03-06 12:20:05 +0400

    Recompiled Pot and Jug to remove duplicate types

--- build.sh
+++ build.sh
@@ -3,10 +3,19 @@
 # TODO: Update this script to be position independant
 
 # Generate Vat contact bindings
-abigen --sol=./dss/src/vat.sol --pkg=maker --type=Vat --out=maker/vat.go
+abigen --sol=./lib/dss/src/vat.sol --pkg=maker --type=Vat --out=maker/vat.go
 
-# Generate Pot contract bindings
-abigen --sol=./dss/src/pot.sol --pkg=maker --type=Pot --alias=Pie=TotalPie --out=maker/pot.go
+# Generate Pot and Jug contracts in two steps to prevent also generating bindings for the DappSys library contracts
+solc --abi --bin --overwrite -o build/ lib/dss/src/pot.sol
+abigen --pkg maker --type Pot \
+  --abi build/Pot.abi \
+  --bin build/Pot.bin \
+  --alias Pie=TotalPie \
+  --out maker/pot.go
 
-# Generate Jug contract bindings
-abigen --sol=./dss/src/jug.sol --pkg=maker --type=Jug --alias=vat=VatAddress,vow=VowAddress --out=./maker/jug.go
+solc --abi --bin --overwrite -o build/ lib/dss/src/jug.sol
+abigen --pkg maker --type Jug \
+  --abi build/Jug.abi \
+  --bin build/Jug.bin \
+  --alias vat=VatAddress,vow=VowAddress \
+  --out maker/jug.go
